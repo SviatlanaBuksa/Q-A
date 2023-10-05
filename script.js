2bf8b79dbbe6c676cbced8fc3c27ab4cf9c3595f
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-10-05 21:47:10 +0200

    Show answer block on click on plus btn

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+"use strict";
 
+let plusBtns = document.querySelectorAll("button");
+let answers = document.querySelectorAll(".answer");
+
+plusBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    let key = Object.keys(plusBtns).find(
+      (k) => plusBtns[k] === e.currentTarget
+    );
+    answers[key].style.display = "block";
+
+    /* btn.children[0].classList.remove("fa-square-plus");
+    btn.children[0].classList.add("fa-square-minus"); */
+  });
+});
