54d2fbce92a44b2cbc5790bcc149265812fc6595
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-10-08 22:16:22 +0200

    Rearrange html, shows answer to all questions

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 "use strict";
 
 let plusBtns = document.querySelectorAll("button");
-let answers = document.querySelectorAll(".answer");
 
 plusBtns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
-    let key = Object.keys(plusBtns).find(
-      (k) => plusBtns[k] === e.currentTarget
-    );
-    answers[key].style.display = "block";
-
-    /* btn.children[0].classList.remove("fa-square-plus");
-    btn.children[0].classList.add("fa-square-minus"); */
+    let allBtns = e.currentTarget.children;
+    for (let btn of allBtns) {
+      btn.classList.toggle("show-answer");
+    }
+    btn.nextElementSibling.classList.toggle("show-answer");
   });
 });
